c2925d5fe541a9935e3726251a1154f71da3adfb
Baalamurgan <baalamurgan2307@gmail.com>
2025-03-12 19:05:45 +0530

    fix: remove validation for signup for admins

--- api/schemas/auth.go
+++ api/schemas/auth.go
@@ -4,12 +4,12 @@ type SignupRequest struct {
 	Username     string `json:"username" validate:"required"`
 	Email        string `json:"email"`
 	Password     string `json:"password"`
-	Phone        string `json:"phone" validate:"required"`
-	AddressLine1 string `json:"address_line_1" validate:"required"`
+	Phone        string `json:"phone"`
+	AddressLine1 string `json:"address_line_1"`
 	AddressLine2 string `json:"address_line_2"`
 	AddressLine3 string `json:"address_line_3"`
-	State        string `json:"state" validate:"required"`
-	Pin          string `json:"pin" validate:"required"`
+	State        string `json:"state"`
+	Pin          string `json:"pin"`
 }
 
 type LoginRequest struct {
